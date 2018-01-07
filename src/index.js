7ff6bee9301242a86cfd5531edecd536f1e88098
Reto Gmür <reto@factsmission.com>
2018-01-07 23:53:07 +0100

    Executing script with graphs as arguments

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Clipboard from "clipboard";
 import $ from "jquery";
+import rdf2h from "rdf2h";
+import rdf from "rdflib";
 import CodeMirror from "codemirror";
 import "codemirror/mode/turtle/turtle"
 import "codemirror/mode/javascript/javascript"
@@ -15,10 +17,10 @@ function align(string) {
     let lines = string.split(/\r?\n/);
     let minMargin = lines.filter(l => l.trim().length > 0)
         .map(value =>
-        value.match(/ */)[0].length)
-        .reduce((a,b) => a < b ? a : b);
+            value.match(/ */)[0].length)
+        .reduce((a, b) => a < b ? a : b);
     return lines.map(l => l.substr(minMargin))
-        .filter((l, pos, array) => !(pos === 0 || pos === array.length-1) && l.trim().length !== 0)
+        .filter((l, pos, array) => !(pos === 0 || pos === array.length - 1) && l.trim().length !== 0)
         .join("\n");
 }
 let vars = {};
@@ -34,7 +36,7 @@ $(".editor").toArray().forEach((editorData) => {
         viewportMargin: Infinity
     });
     cmEditor.setValue(align(value));
-    let copyLink = $("<a href='#'>copy as JS-String</a>");
+    let copyLink = $("<a href='#' >copy as JS-String</a>");
     copyLink.insertAfter(editorDiv);
     new Clipboard(copyLink[0], {
         text: function (trigger) {
@@ -42,8 +44,37 @@ $(".editor").toArray().forEach((editorData) => {
                 .split("\n").join("\\n\\\n") + "\'";
         }
     });
+    if (type === "application/javascript") {
+        let currentVars = {};
+        Object.keys(vars).forEach(key => currentVars[key] = vars[key]);
+        let executeLink = $("<a href='#'>execute</a> ");
+        executeLink.insertAfter(editorDiv);
+        let resultAreas = new Array();
+        executeLink.click(() => {
+            resultAreas.forEach(a => a.remove());
+            let result;
+            try {
+                result = new Function("rdf2h", ...Object.keys(currentVars), cmEditor.getValue())
+                    (rdf2h, ...Object.values(currentVars).map(v => v()));
+            } catch (err) {
+                let stackLines = err.stack.split("\n");
+                let lineWithSelf = stackLines.findIndex(l => l.indexOf("at eval") > 0);
+                err.stack = stackLines.splice(0, lineWithSelf).join("\n");
+                throw err;
+            }
+            let resultArea = $("<div class='result'>Returns the following result:<br>\
+            <code class='result'>"+result+"</code></div>");
+            resultArea.insertAfter(editorDiv);
+            resultAreas.push(resultArea);
+        });
+    }
     $("<br/>").insertAfter(copyLink);
-    //vars()
-console.log($(editorData).attr("class").split(" ").filter(c => c !== "editor"));
+    let additionalClasses = $(editorData).attr("class").split(" ").filter(c => c !== "editor")
+    additionalClasses.forEach(name => vars[name] = () => 
+    {
+        var graph = rdf.graph();
+        rdf.parse(cmEditor.getValue(), graph, "http://example.org/"+name+"/", type);
+        return graph;
+    });
 });
 $(" .CodeMirror").css("height", "auto");
