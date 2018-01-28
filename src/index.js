60c4a8651aa1a2fb042c87d28f2357f16be78e53
Noam Bachmann <noam@factsmission.com>
2018-01-28 16:21:02 +0100

    appearance of the buttons and some details improved

--- src/index.js
+++ src/index.js
@@ -42,14 +42,14 @@ $(".editor").toArray().forEach((editorData) => {
         viewportMargin: Infinity
     });
     cmEditor.setValue(align(value));
-    let copyJsLink = $("<button width='24' height='24'><svg style='width:24px;height:24px' viewBox='0 0 24 24'><path d='M 18,14 A 1,1 0 0 1 17.292893,13.707107 1,1 0 0 1 17,13' fill='none' stroke='#000' stroke-miterlimit='3.6508' stroke-width='2'/><path d='m17 11a1 1 0 0 1 1 -1' fill='none' stroke='#000' stroke-miterlimit='3.6508' stroke-width='2'/><path d='m3 17v-14' fill='none' stroke='#000' stroke-linejoin='round' stroke-miterlimit='3.6508' stroke-width='2'/><path d='m4 2h12' fill='none' stroke='#000' stroke-linejoin='round' stroke-miterlimit='3.6508' stroke-width='2'/><path d='m8 6h13' fill='none' stroke='#000' stroke-linejoin='round' stroke-miterlimit='3.6508' stroke-width='2'/><path d='m7 7v14' fill='none' stroke='#000' stroke-linejoin='round' stroke-miterlimit='3.6508' stroke-width='2'/><path d='m14 9v8' fill='none' stroke='#000' stroke-linejoin='round' stroke-miterlimit='3.6508' stroke-width='2'/><path d='m21 10h-3' fill='none' stroke='#000' stroke-linejoin='round' stroke-miterlimit='3.6508' stroke-width='2'/><path d='m17 11v2' fill='none' stroke='#000' stroke-linejoin='round' stroke-miterlimit='3.6508' stroke-width='2'/><path d='m20 15v2' fill='none' stroke='#000' stroke-linejoin='round' stroke-miterlimit='3.6508' stroke-width='2'/><path d='m19 18h-3' fill='none' stroke='#000' stroke-linejoin='round' stroke-miterlimit='3.6508' stroke-width='2'/><path d='m8 22h13' fill='none' stroke='#000' stroke-linejoin='round' stroke-miterlimit='3.6508' stroke-width='2'/><path d='m11 15v2' fill='none' stroke='#000' stroke-linejoin='round' stroke-miterlimit='3.6508' stroke-width='2'/><path d='m20 17a1 1 0 0 1 -1 1' fill='none' stroke='#000' stroke-miterlimit='3.6508' stroke-width='2'/><path d='m14 17a1 1 0 0 1 -1 1' fill='none' stroke='#000' stroke-miterlimit='3.6508' stroke-width='2'/><path d='M 8,22 A 1,1 0 0 1 7.2928932,21.707107 1,1 0 0 1 7,21' fill='none' stroke='#000' stroke-miterlimit='3.6508' stroke-width='2'/><path d='M 12,18 A 1,1 0 0 1 11.292893,17.707107 1,1 0 0 1 11,17' fill='none' stroke='#000' stroke-miterlimit='3.6508' stroke-width='2'/><path d='m7 7a1 1 0 0 1 1 -1' fill='none' stroke='#000' stroke-miterlimit='3.6508' stroke-width='2'/><path d='m3 3a1 1 0 0 1 1 -1' fill='none' stroke='#000' stroke-miterlimit='3.6508' stroke-width='2'/><path d='m19 14a1 1 0 0 1 1 1' fill='none' stroke='#000' stroke-miterlimit='3.6508' stroke-width='2'/><path d='m13 18h-1' fill='none' stroke='#000' stroke-linejoin='round' stroke-miterlimit='3.6508' stroke-width='2'/><path d='m19 14h-1' fill='none' stroke='#000' stroke-linejoin='round' stroke-miterlimit='3.6508' stroke-width='2'/></svg></button>");
+    let copyJsLink = $("<button class='btn-svg' width='24' height='24'><svg style='width:24px;height:24px' viewBox='0 0 24 24'><path d='m 16,13 c 0,0.530049 0.211136,1.039261 0.585938,1.414062 C 16.960739,14.788864 17.469951,15 18,15 v -2 z' /><path d='m 18,9 c -1.092725,0 -2,0.9072752 -2,2 h 2 z' /><path d='M 2,3 V 17 H 4 V 3 Z' /><path d='M 4,1 V 3 H 16 V 1 Z' /><path d='M 8,5 V 7 H 21 V 5 Z' /><path d='M 6,7 V 21 H 8 V 7 Z' /><path d='m 13,9 v 8 h 2 V 9 Z' /><path d='m 18,9 v 2 h 3 V 9 Z' /><path d='m 16,11 v 2 h 2 v -2 z' /><path d='m 19,15 v 2 h 2 v -2 z' /><path d='m 16,17 v 2 h 3 v -2 z' /><path d='m 8,21 v 2 h 13 v -2 z' /><path d='m 10,15 v 2 h 2 v -2 z' /><path d='m 19,17 v 2 c 1.092725,0 2,-0.907275 2,-2 z' /><path d='m 13,17 v 2 c 1.092725,0 2,-0.907275 2,-2 z' /><path d='m 6,21 c 0,0.530049 0.2111364,1.039261 0.5859375,1.414062 C 6.9607386,22.788864 7.4699512,23 8,23 v -2 z' /><path d='m 10,17 c 0,0.530049 0.211136,1.039261 0.585938,1.414062 C 10.960739,18.788864 11.469951,19 12,19 v -2 z' /><path d='M 8,5 C 6.9072752,5 6.0000001,5.9072752 6,7 h 2 z' /><path d='m 4,1 c -1.0927248,-2e-8 -1.9999999,0.9072752 -2,2 h 2 z' /><path d='m 19,13 v 2 h 2 c 0,-1.092725 -0.907275,-2 -2,-2 z' /><path d='m 12,17 v 2 h 1 v -2 z' /><path d='m 18,13 v 2 h 1 v -2 z' /></svg></button>");
     copyJsLink.insertAfter(editorDiv);
     new Clipboard(copyJsLink[0], {
         text: function (trigger) {
             return  cmEditor.getValueAsJS();
         }
     });
-    let copyLink = $("<button width='24' height='24'><svg style='width:24px;height:24px' viewBox='0 0 24 24'><path fill='#000' d='M19,21H8V7H19M19,5H8A2,2 0 0,0 6,7V21A2,2 0 0,0 8,23H19A2,2 0 0,0 21,21V7A2,2 0 0,0 19,5M16,1H4A2,2 0 0,0 2,3V17H4V3H16V1Z'/></svg></button>")
+    let copyLink = $("<button class='btn-svg' width='24' height='24'><svg style='width:24px;height:24px' viewBox='0 0 24 24'><path d='M19,21H8V7H19M19,5H8A2,2 0 0,0 6,7V21A2,2 0 0,0 8,23H19A2,2 0 0,0 21,21V7A2,2 0 0,0 19,5M16,1H4A2,2 0 0,0 2,3V17H4V3H16V1Z'/></svg></button>")
     copyLink.insertAfter(editorDiv);
     new Clipboard(copyLink[0], {
         text: function (trigger) {
@@ -60,7 +60,7 @@ $(".editor").toArray().forEach((editorData) => {
     if (type === "application/javascript") {
         let currentVars = {};
         Object.keys(vars).forEach(key => currentVars[key] = vars[key]);
-        let executeLink = $("<button width='24' height='24'><svg width='24' height='24' viewBox='0 0 24 24'><path d='M17.65,6.35C16.2,4.9 14.21,4 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20C15.73,20 18.84,17.45 19.73,14H17.65C16.83,16.33 14.61,18 12,18A6,6 0 0,1 6,12A6,6 0 0,1 12,6C13.66,6 15.14,6.69 16.22,7.78L13,11H20V4L17.65,6.35Z' /></svg></button> ");
+        let executeLink = $("<button class='btn-svg' width='24' height='24'><svg width='24' height='24' viewBox='0 0 24 24'><path d='M17.65,6.35C16.2,4.9 14.21,4 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20C15.73,20 18.84,17.45 19.73,14H17.65C16.83,16.33 14.61,18 12,18A6,6 0 0,1 6,12A6,6 0 0,1 12,6C13.66,6 15.14,6.69 16.22,7.78L13,11H20V4L17.65,6.35Z' /></svg></button> ");
         executeLink.insertAfter(editorDiv);
         let resultAreas = new Array();
         function execute() {
@@ -100,7 +100,7 @@ $(".editor").toArray().forEach((editorData) => {
 });
 $(" .CodeMirror").css("height", "auto");
 
-let createTestLink = $("<a class='copyAsTest' href='#'><svg width='24' height='24' viewBox='0 0 24 24'><path d='M14.6,16.6L19.2,12L14.6,7.4L16,6L22,12L16,18L14.6,16.6M9.4,16.6L4.8,12L9.4,7.4L8,6L2,12L8,18L9.4,16.6Z' /></svg></a>");
+let createTestLink = $("<br><button class='copyAsTest btn-svg'><svg width='24' height='24' viewBox='0 0 24 24'><path d='M14.6,16.6L19.2,12L14.6,7.4L16,6L22,12L16,18L14.6,16.6M9.4,16.6L4.8,12L9.4,7.4L8,6L2,12L8,18L9.4,16.6Z' /></svg></button>");
 new Clipboard(".copyAsTest", {
     text: function (trigger) {
         let section = $(event.target.closest(".step"));
